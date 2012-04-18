8c38c454ce9288464893cd06201c5e826ceae753
Menion Asamm <menion.asamm@gmail.com>
2012-04-18 11:39:51 +0200

    - added part of code that help to work on tablets (AR add-on)

--- LocusAddonAR/src/menion/android/locus/addon/ar/Main.java
+++ LocusAddonAR/src/menion/android/locus/addon/ar/Main.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2012, Asamm Software, s. r. o.
+ * 
+ * This file is part of Locus - add-on AR project (LocusAddonAR).
+ *
+ * This program is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Lesser General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 package menion.android.locus.addon.ar;
 
 import java.util.List;
@@ -74,7 +90,7 @@ public class Main extends Activity implements SensorEventListener, OnTouchListen
 		
 		final PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
 		this.mWakeLock = pm.newWakeLock(
-				PowerManager.SCREEN_BRIGHT_WAKE_LOCK, "My Tag");
+				PowerManager.SCREEN_BRIGHT_WAKE_LOCK, TAG);
 
 		// initialize imageButtons
 		ImageButton btnHome = (ImageButton) findViewById(R.id.btn_home);
@@ -151,8 +167,13 @@ public class Main extends Activity implements SensorEventListener, OnTouchListen
 			// clear all events
 			aView.clearEvents();
 
+			// Phone
+//			double angleX = Math.toRadians(-90);
+//			double angleY = Math.toRadians(-90);
+			
+			// Tablet
 			double angleX = Math.toRadians(-90);
-			double angleY = Math.toRadians(-90);
+			double angleY = Math.toRadians(0);
 
 			m1.set(1f, 0f, 0f,
 					0f, (float) Math.cos(angleX), (float) -Math.sin(angleX),
